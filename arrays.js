1e1c9f9018e203f9068f4f276fb0e401000a781b
Celia Gearin <cgearin18@gmail.com>
2018-05-10 10:51:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,8 +2,8 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
   [element, ...array];
 }
-function destructivelyAddElementToBeginningOfArray(a, b) {
-  a.unshift(b);
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
   return a;
 }
 function addElementToEndOfArray(a, b) {
