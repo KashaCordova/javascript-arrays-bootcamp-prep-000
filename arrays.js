b4d74c10e53801c0f961e72e2134a1154f3ff6b3
Celia Gearin <cgearin18@gmail.com>
2018-05-10 11:05:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
   var newArray = [element, ...array];
-  return newArray;
+  console.log(newArray);
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
