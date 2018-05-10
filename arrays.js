df6d0b4e6123e0d9fd485a482dffe1294fd0f65a
Celia Gearin <cgearin18@gmail.com>
2018-05-10 10:08:05 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,32 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+function addElementToBeginningOfArray(a, b) {
+  var newArray = a.unshift(b);
+  return newArray;
+}
+function destructivelyAddElementToBeginningOfArray(a, b) {
+  a.unshift(b);
+  return a;
+}
+function addElementToEndOfArray(a, b) {
+  var newPushArray = a.push(b);
+  return newPushArray;
+}
+function destructivelyAddElementToEndOfArray(a, b) {
+  a.push(b);
+  return a;
+}
+function accessElementInArray(a, b) {
+  return a[b];
+}
+function destructivelyRemoveElementFromBeginningofArray(a) {
+  a.shift;
+  return a;
+}
+function removeElementFromBeginningOfArray(a) {
+var slice = a.slice(1);
+return slice;
+}
+function destructivelyRemoveElementFromEndOfArray(a) {
+  a.pop();
+  return a;
+}
